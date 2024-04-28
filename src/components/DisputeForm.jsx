2c68b7e05b1a7ecacc5f1d38fd433e8a6c6bdff5
Chetan2811 <52730563+Chetan2811@users.noreply.github.com>
2024-04-28 12:13:42 -0400

    Created disputeform

---
+++ src/components/DisputeForm.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import  { useState } from 'react';
+import { collection, addDoc } from "firebase/firestore";
+import { firestore } from "../firebase";
+
+
+const DisputeForm = ({data, user, toggle}) => {
+  const [issue, setIssue] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const uid = data.cid;
+    const oid = data.oid;
+    const bid = data.id;
+    const raisedBy = user.uid;
+
+    
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <textarea
+          value={issue}
+          onChange={(e) => setIssue(e.target.value)}
+          className="p-2 border rounded"
+          placeholder="Describe the issue"
+          rows="4"
+          required
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">
+          Submit Dispute
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default DisputeForm;
